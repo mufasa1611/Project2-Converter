7f13254e5dd2827ce4fc1f0a5f352705199b2f6e
Mohamed Abdel Ghany <mufasa1611@gmail.com>
2023-09-15 16:02:44 +0000

    creat js file, define variables

---
+++ assets/convert.js
@@ -0,0 +1,8 @@
+//  defined variables
+let unitSelect = document.getElementById('unit-sys'); //   element that contains the unit system dropdown list
+let fromUnitGroup = document.getElementById('from-unit-box');  // group element that contains the (from units) dropdown list
+let toUnitGroup = document.getElementById('to-unit-box');  // group element that contains the (to units) dropdown list
+let fromUnitSelect = document.getElementById('from-unit'); // The (from unit) dropdown select 
+let toUnitSelect = document.getElementById('to-unit'); // The (to unit) dropdown select
+const Input = document.getElementById('input'); // The input field where the user enters the value to be converted
+const output = document.getElementById('output'); // The output field where the result is displayed
